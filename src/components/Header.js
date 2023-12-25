02cf675e150ba97eb4cc59b4de4f5a9dfacb9441
llldasha <del5@tpu.ru>
2023-12-26 00:27:00 +0700

    помолимся

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,53 @@
-import React, { Component } from "react";
-import { Button, Container, FormControl, NavLink, Navbar, NavbarCollapse, NavbarToggle, Nav, Form } from "react-bootstrap";
-import logo from "./logo.png";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Contacts from '../pages/Contacts';
+import React, { Component } from 'react'
+import { Button, Container, Navbar, NavbarBrand, NavbarCollapse, Nav, Form, FormControl } from 'react-bootstrap'
+import logo from './logo.png';
+
+import{BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import Home from '../Pages/Home';
+import Cards from '../pages/Cards';
+import Quiz from '../pages/Quiz';
+import Theory from '../pages/Theory';
+
 
 
 export default class Header extends Component {
+
     render() {
         return (
             <>
-                <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Navbar sticky ="top" collapseOnSelect expand="md" bg="dark" variant='dark'>
                     <Container>
-                        <Navbar.Brand href="/">
+                        <Navbar.Brand href="/" >
                             <img
                                 src={logo}
-                                height="30"
-                                width="30"
+                                height="40"
+                                width="40"
                                 className="d-inline-block align-top"
-
-                                alt="logo"
-
+                                alt="Logo"
                             />
+                         Привет,Наташка!
 
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="mr-auto">
+                                
                                 <Nav.Link href="/">Главная страница</Nav.Link>
-                                <Nav.Link href="/about">О нас</Nav.Link>
-                                <Nav.Link href="/contacts">Контакты</Nav.Link>
-
 
                             </Nav>
-                            <Form className='d-flex'>
-                            <Button variant="outline-info">Регистрация</Button>
-                            </Form>
 
                         </Navbar.Collapse>
                     </Container>
-
-
                 </Navbar>
-                <Router>
-                    <Switch>
-                        <Route exect path="/" component={Home}/>
-                        <Route exect path="/about" component={About}/>
-                        <Route exect path="/contacts" component={Contacts}/> 
-                    </Switch>
+                 <Router>
+                <Routes>
+                        <Route path='/' element={<Home/>}/>
+                    </Routes>
                 </Router>
             </>
+           
+
         )
     }
+
 }
