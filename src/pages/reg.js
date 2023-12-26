1dccc4f58a44e20d796bd3806a2fb556d954e144
llldasha <johndoe@example.com>
2023-12-26 22:39:59 +0700

    начала главную

---
+++ src/pages/reg.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+export default class Reg extends Component{
+    render(){
+        return(
+            <div>
+                Hello Home
+            </div>
+        )
+    }
+}
